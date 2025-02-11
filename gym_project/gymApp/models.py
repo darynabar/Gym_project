b4108ec0a9b371cda6967af7fa96e4036b881070
0sadets <102857086+0sadets@users.noreply.github.com>
2025-02-11 20:05:57 +0200

    fixed db models, add auto calculation for UserMembership date

--- gym_project/gymApp/models.py
+++ gym_project/gymApp/models.py
@@ -6,6 +6,20 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+# Абонементи
+class Membership(models.Model):
+    name = models.CharField(max_length=100)
+    DURATION_CHOICES = [
+        ("monthly", "Місячний"),
+        ("yearly", "Річний"),
+        ("one-time", "Разовий"),
+    ]
+    duration = models.CharField(max_length=10, choices=DURATION_CHOICES)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} ({self.get_duration_display()})"
+
 class Trainer(models.Model):
    # user = models.ForeignKey(User, related_name='memberships', on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
@@ -30,15 +44,6 @@ class Service(models.Model):
     def __str__(self):
         return f"{self.name} ({self.trainer})"
     
-# зв'язок користувача з абонементами
-class UserMembership(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="memberships")
-    membership = models.ForeignKey(Membership, on_delete=models.CASCADE)
-    start_date = models.DateField(auto_now_add=True)
-    end_date = models.DateField()  # закінчення абонемента
-
-    def __str__(self):
-        return f"{self.user} - {self.membership} (до {self.end_date})"
 
 # зв'язок користувача з послугами
 class UserService(models.Model):
@@ -49,19 +54,7 @@ class UserService(models.Model):
     def __str__(self):
         return f"{self.user} - {self.service}"
 
-# Абонементи
-class Membership(models.Model):
-    name = models.CharField(max_length=100)
-    DURATION_CHOICES = [
-        ("monthly", "Місячний"),
-        ("yearly", "Річний"),
-        ("one-time", "Разовий"),
-    ]
-    duration = models.CharField(max_length=10, choices=DURATION_CHOICES)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
 
-    def __str__(self):
-        return f"{self.name} ({self.get_duration_display()})"
 
 # Розклад занять
 class Schedule(models.Model):
@@ -73,7 +66,7 @@ class Schedule(models.Model):
     def __str__(self):
         return f"{self.service.name} з {self.trainer} ({self.date_time})"
 
-# GymBar (додаткові товари або напої)
+
 class GymBar(models.Model):
     CATEGORY_CHOICES = [
         ("protein", "Протеїновий коктейль"),
@@ -83,15 +76,34 @@ class GymBar(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
-    volume = models.CharField(max_length=50, null=True, blank=True)  # Об'єм (наприклад, 500 мл)
+    volume = models.CharField(max_length=50, null=True, blank=True)  
 
     def __str__(self):
         return f"{self.name} ({self.get_category_display()})"
 
-# Зали (Halls)
 class Hall(models.Model):
     name = models.CharField(max_length=100)
     capacity = models.IntegerField()
 
     def __str__(self):
         return self.name
+    
+# зв'язок користувача з абонементами
+class UserMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="memberships")
+    membership = models.ForeignKey(Membership, on_delete=models.CASCADE)
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField() 
+
+    def save(self, *args, **kwargs):
+        if not self.end_date:
+            if self.membership.duration == "monthly":
+                self.end_date = self.start_date + timedelta(days=30)
+            elif self.membership.duration == "yearly":
+                self.end_date = self.start_date + timedelta(days=365)
+            elif self.membership.duration == "one-time":
+                self.end_date = self.start_date 
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user} - {self.membership} (до {self.end_date})"
