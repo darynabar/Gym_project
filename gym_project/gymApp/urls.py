fde0ef4877d934e80fe98051b8205e0d22eda95d
0sadets <102857086+0sadets@users.noreply.github.com>
2025-02-13 01:47:47 +0200

    create components, add changes to urls.py

--- gym_project/gymApp/urls.py
+++ gym_project/gymApp/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', views.index),
+    path('', views.index),
     path('coach/', views.coach),
 ]
