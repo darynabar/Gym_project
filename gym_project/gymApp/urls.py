4705815b69833164e55f9a0f98a771b45d1e84fc
0sadets <102857086+0sadets@users.noreply.github.com>
2025-02-13 14:44:54 +0200

    create gym_schedule page, connect botstrap, fix styles, fix header

--- gym_project/gymApp/urls.py
+++ gym_project/gymApp/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index),
-    path('coach/', views.coach),
+    path('', views.index, name="index"),
+    path('coach/', views.coach, name="coach"),
+    path('gym_schedule/', views.gym_schedule, name="gym_schedule"),
 ]
