80a213a52d62d62153b265d6c73927ab952468bc
0sadets <102857086+0sadets@users.noreply.github.com>
2025-02-15 00:37:49 +0200

    create schedule table, fixed some styles

--- gym_project/gym_project/settings.py
+++ gym_project/gym_project/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gymApp'
+    'gymApp',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +103,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"), 
-]
+# STATICFILES_DIRS = [
+#      BASE_DIR / "static", 
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
