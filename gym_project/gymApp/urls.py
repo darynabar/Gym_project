83db90409b304f37d2d59182cdd38e877d674bb5
0sadets <102857086+0sadets@users.noreply.github.com>
2025-02-21 01:30:32 +0200

    set view for service_info && style scrollbar

--- gym_project/gymApp/urls.py
+++ gym_project/gymApp/urls.py
@@ -1,13 +1,14 @@
 from gymApp import views
 from django.contrib import admin
 from django.urls import path, include
-from .views import trainers_list, trainer_detail
+from .views import service_info, trainers_list, trainer_detail
 urlpatterns = [
     path('', views.index, name="index"),
     path('coach/', views.coach, name="coach"),
     path('gym_schedule/', views.gym_schedule, name="gym_schedule"),
     path("trainers/", trainers_list, name="trainers_list"),
     path("trainers/<int:trainer_id>/", trainer_detail, name="trainer_detail"),
+    path('service/<int:id>/', service_info, name='service_info'),
 ]
 
 
